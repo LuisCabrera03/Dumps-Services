dd0236ddfd1469338aa2e4235f3fe1d54c279b32
kevincab09 <153546811+PabloSerrato@users.noreply.github.com>
2024-05-18 09:52:17 -0500

    werwr
    asd

--- Components/home.php
+++ Components/home.php
@@ -3,9 +3,9 @@
     <h1 class="titulo">¡Bienvenido a Dump Services!</h1>
 </div>
 
-<div class="container centered"> <!-- Contenedor principal -->
-    <div class="column"> <!-- Columna para el texto -->
-        <div class="textos"> <!-- Contenedor para los textos -->
+<div class="container centered">
+    <div class="row">
+        <div class="col-md-6">
             <div class="texto-descripcion">
                 <p>
                     Dump Services es una plataforma dedicada a proporcionar soluciones de transporte utilizando motocargueros. Nos enfocamos en ofrecer un servicio eficiente y confiable para satisfacer las necesidades de nuestros clientes. Nuestro objetivo es facilitar el transporte de mercancías de manera rápida, segura y económica.
@@ -16,6 +16,11 @@
                 <p>
                     Nuestro compromiso es optimizar el proceso de transporte y logística, ofreciendo soluciones innovadoras y adaptadas a las necesidades del mercado actual.
                 </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="video-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
@@ -38,13 +43,77 @@
 <div class="container mt-5">
     <div class="row img-container">
         <div class="col-md-4">
-            <img src="https://www.noticiasllaneras.com/wp-content/uploads/2020/07/Moto-cargueros-720x445.jpg" alt="Imagen 1" class="img-fluid img-hover">
+            <img src="https://www.noticiasllaneras.com/wp-content/uploads/2020/07/Moto-cargueros-720x445.jpg" alt="Imagen 1" class="img-fluid img-hover custom-img">
         </div>
         <div class="col-md-4">
-            <img src="https://www.revistaautocrash.com/wp-content/uploads/2022/09/Karguero-6-1024x683.jpg" alt="Imagen 2" class="img-fluid img-hover">
+            <img src="https://www.revistaautocrash.com/wp-content/uploads/2022/09/Karguero-6-1024x683.jpg" alt="Imagen 2" class="img-fluid img-hover custom-img">
         </div>
         <div class="col-md-4">
-            <img src="https://mas.autecomobility.com/wp-content/uploads/2022/11/motocarro-auteco-829x550.jpg" alt="Imagen 3" class="img-fluid img-hover">
+            <img src="https://mas.autecomobility.com/wp-content/uploads/2022/11/motocarro-auteco-829x550.jpg" alt="Imagen 3" class="img-fluid img-hover custom-img">
         </div>
     </div>
 </div>
+
+
+
+
+<div class="container mt-5">
+        <h2 class="text-center mb-4">Testimonios de nuestros clientes</h2>
+        <div class="img-container">
+            <div class="testimonial-item">
+                <img src="https://blog.ida.cl/wp-content/uploads/sites/5/2019/06/clientesnosabenquequieren.png"
+                    alt="Cliente 1" class="testimonial-img" data-toggle="tooltip" data-placement="top"
+                    title="¡Excelente servicio! Me ayudaron a enviar mis productos de forma rápida y segura.">
+                <div class="testimonial-text">
+                    ¡Excelente servicio! Me ayudaron a enviar mis productos de forma rápida y segura.
+                </div>
+            </div>
+            <div class="testimonial-item">
+                <img src="https://blog.ida.cl/wp-content/uploads/sites/5/2019/06/clientesnosabenquequieren.png"
+                    alt="Cliente 2" class="testimonial-img" data-toggle="tooltip" data-placement="top"
+                    title="¡Muy recomendado! Me sorprendió lo eficiente que fue el proceso de envío.">
+                <div class="testimonial-text">
+                    ¡Muy recomendado! Me sorprendió lo eficiente que fue el proceso de envío.
+                </div>
+            </div>
+            <div class="testimonial-item">
+                <img src="https://blog.ida.cl/wp-content/uploads/sites/5/2019/06/clientesnosabenquequieren.png"
+                    alt="Cliente 3" class="testimonial-img" data-toggle="tooltip" data-placement="top"
+                    title="¡Fantástico servicio al cliente! Siempre están disponibles para responder a mis preguntas.">
+                <div class="testimonial-text">
+                    ¡Fantástico servicio al cliente! Siempre están disponibles para responder a mis preguntas.
+                </div>
+            </div>
+            <div class="testimonial-item">
+                <img src="https://blog.ida.cl/wp-content/uploads/sites/5/2019/06/clientesnosabenquequieren.png"
+                    alt="Cliente 4" class="testimonial-img" data-toggle="tooltip" data-placement="top"
+                    title="¡Excelente experiencia! El envío llegó en perfectas condiciones y en el tiempo esperado.">
+                <div class="testimonial-text">
+                    ¡Excelente experiencia! El envío llegó en perfectas condiciones y en el tiempo esperado.
+                </div>
+            </div>
+            <div class="testimonial-item">
+                <img src="https://blog.ida.cl/wp-content/uploads/sites/5/2019/06/clientesnosabenquequieren.png"
+                    alt="Cliente 5" class="testimonial-img" data-toggle="tooltip" data-placement="top"
+                    title="¡Increíble servicio! Me ayudaron con mi envío de manera eficiente y sin complicaciones.">
+                <div class="testimonial-text">
+                    ¡Increíble servicio! Me ayudaron con mi envío de manera eficiente y sin complicaciones.
+                </div>
+            </div>
+            <div class="testimonial-item">
+                <img src="https://blog.ida.cl/wp-content/uploads/sites/5/2019/06/clientesnosabenquequieren.png"
+                    alt="Cliente 6" class="testimonial-img" data-toggle="tooltip" data-placement="top"
+                    title="¡Muy satisfecho! El proceso de envío fue rápido y fácil.">
+                <div class="testimonial-text">
+                    ¡Muy satisfecho! El proceso de envío fue rápido y fácil.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
