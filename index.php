a9cd997d02f0f442ba969381760fa69140b10d91
PabloSerrato <pabloserrato2018@gmail.com>
2024-08-28 16:00:13 -0500

    base de datos

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css\main.css"> 
+    <link rel="icon" href="fonts\imagenes\logo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> <!-- Enlace a Font Awesome -->
     <title>Dump Services</title>
 </head>
